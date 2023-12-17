18f242abd22d9838619dfe5535f468cf464f6e6a
Kaltuma777 <Kaltuma.abdi@student.moringaschool.com>
2023-12-17 16:44:26 +0300

    completed

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Filter({ onCategoryChange }) {
+// Filter component for searching and filtering by category
+function Filter({ onCategoryChange, onSearchChange }) {
+  const [searchText, setSearchText] = useState("testing");
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleSearchChange(event) {
+    const newSearchText = event.target.value;
+    setSearchText(newSearchText);
+    onSearchChange(newSearchText);
+  }
+
+  function handleCategoryChange(event) {
+    const newSelectedCategory = event.target.value;
+    setSelectedCategory(newSelectedCategory);
+    onCategoryChange(newSelectedCategory);
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <label>
+        Search:
+        <input
+          type="text"
+          name="search"
+          placeholder="Search..."
+          value={searchText}
+          onChange={handleSearchChange}
+        />
+      </label>
+      <select
+        name="filter"
+        value={selectedCategory}
+        onChange={handleCategoryChange}
+      >
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [itemName, setItemName] = useState("");
+  const [itemCategory, setItemCategory] = useState("Produce");
+
+  function handleNameChange(event) {
+    setItemName(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    setItemCategory(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const newItem = {
+      id: uuid(),
+      name: itemName,
+      category: itemCategory,
+    };
+
+    onItemFormSubmit(newItem);
+
+    setItemName("");
+    setItemCategory("Produce");
+  }
+
   return (
-    <form className="NewItem">
+    <form onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" value={itemName} onChange={handleNameChange} />
       </label>
-
       <label>
         Category:
-        <select name="category">
+        <select value={itemCategory} onChange={handleCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
+          {/* Add more options as needed */}
         </select>
       </label>
-
-      <button type="submit">Add to List</button>
+      <button type="submit">Add Item</button>
     </form>
   );
 }
